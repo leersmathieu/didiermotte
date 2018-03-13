4ab6c7f5ccbb8f9ca5e7c0897d814d7d8414c8fb
leersmathieu <leers.mathieu@gmail.com>
2018-03-13 15:55:10 +0100

    structure and design preface

---
+++ views/head.php
@@ -0,0 +1,14 @@
+<section class="head" >
+    <div class="vcard">
+        <img src="views/assets/img/didier.png" alt="Didier Motte">
+        <p>Antiquités • Décoration • Aménagements d'intérieur</p>
+        <p>☎<a href="tel:+32495205934" title="Cliquez pour lancer un appel téléphonique: +32 495/20.59.34">+32 495/20.59.34</a></p>
+        <nav role="navigation">
+            <ul>
+                <li><a href="https://didiermotte.be/didier-motte-2/">Présentation</a></li>
+                <li><a href="https://didiermotte.be/stock/">Le stock</a></li>
+                <li><a href="https://didiermotte.be/contact/">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</section>
---
+++ views/stock.php
@@ -0,0 +1,8 @@
+<section class="stock" >
+    <h1>le stock</h1>
+    <form method="get" name="searchstock" action="">
+        <label for="search"> fouiller le stock </label>
+        <input name="search" value="" type="text" autocapitalize="off" autocorrect="off" placeholder="Exemple: table"> 
+        <input type="submit" class="button" value="recherche">
+    </form>
+</section>
